4b66bae065baa7a797c56420d5867fedfe0430be
byronrsa-glitch <byron.rsa@gmail.com>
2026-03-17 00:30:07 +0200

    Create config.js file with customizable configuration

---
+++ config.js
@@ -0,0 +1,13 @@
+const config = {
+  storeName: 'Your Store Name',
+  brands: [],
+  categories: [],
+  colors: [],
+  shopifyApi: {
+    apiKey: 'your_api_key',
+    password: 'your_api_password',
+    storeUrl: 'your_store_url',
+  }
+};
+
+module.exports = config;
